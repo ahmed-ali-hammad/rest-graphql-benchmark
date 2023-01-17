914217c6eac7f67b3ad6083ba98827f4b2bc3ef9
Ahmed Hammad <ahmed.hammad.am@gmail.com>
2023-01-17 23:32:26 +0100

    fix csrf token for graphql

--- patient_info/patient_info_graphql/urls.py
+++ patient_info/patient_info_graphql/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 app_name = "patient_info_graphql"
 
 urlpatterns = [
-    path('patient/', GraphQLView.as_view(graphiql=True)),
+    path('patient/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
